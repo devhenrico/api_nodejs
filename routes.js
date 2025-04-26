6665a5a0898cb0d43fd73fe4d681f1b88780e216
Henrico Santos <henricosantos27@outlook.com>
2025-04-26 19:42:31 -0300

    update routes

--- routes.js
+++ routes.js
@@ -17,7 +17,7 @@ export function router() {
     email: z.string().email({ message: "Invalid email address" }),
     password: z
       .string()
-      .min(6, { message: "Password must be at least 6 characters" }),
+      .length(6, { message: "Password must be exactly 6 characters" }),
   });
 
   const userQuerySchema = z
